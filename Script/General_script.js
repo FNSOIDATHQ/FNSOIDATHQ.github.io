df540277f3d646ff8064a1241aae39762d01b8b7
dev gao <59720169+FNSOIDATHQ@users.noreply.github.com>
2023-03-29 22:48:25 +0800

    upload_FAQ_20230329

--- Script/General_script.js
+++ Script/General_script.js
@@ -1,11 +1,32 @@
 // 当网页向下滑动 20px 出现"返回顶部" 按钮
 window.onscroll = function() {scrollFunction()};
  
-function scrollFunction() {console.log(121);
+function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("RTop").style.display = "block";
+        document.getElementsByClassName("float_left")[0].style.display = "flex";
     } else {
         document.getElementById("RTop").style.display = "none";
+        document.getElementsByClassName("float_left")[0].style.display = "none";
+    }
+}
+
+function nav_control(){
+    if($('.nav_float').is(":visible")){
+        $('.divide_nav').hide();
+        $('.nav_float').hide();
+        $('.nav_button').css('border-top-right-radius','10px');
+        $('.nav_button').css('border-bottom-right-radius','10px');
+        $('.nav_button').css('max-width','50px');
+        $('.nav_button').text('目录');
+    }
+    else{
+        $('.divide_nav').show();
+        $('.nav_float').show();
+        $('.nav_button').css('border-top-right-radius','0px');
+        $('.nav_button').css('border-bottom-right-radius','0px');
+        $('.nav_button').css('max-width','20px');
+        $('.nav_button').text('收起');
     }
 }
  
@@ -16,4 +37,4 @@ function topFunction() {
 }
 
 //啊哈，我根本就不想写甚么“JS”脚本，至少现在我讨厌这个东西，即使听说它很好用，实际上也很简单
-//理论上，我们还需要一个可以收起的导航目录，而不是像个蠢货一样把目录堆在最开头
+//好，现在有了一个简单的导航目录 =)
